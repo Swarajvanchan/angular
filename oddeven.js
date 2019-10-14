072c0eb1f89a0ae9b84f7e3d33964942f4bd3d26
Swarajvanchan <swarajvanchan@gmail.com>
2019-10-14 14:23:41 +0530

    js

---
+++ oddeven.js
@@ -0,0 +1,11 @@
+function even_odd(num){
+    var num;
+     if (num%2==0) {
+         console.log("The Number is even number");
+         
+     } else {
+         console.log("Odd number");
+         
+     }
+ }
+ even_odd(10)
