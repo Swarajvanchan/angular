4705168e2d586731ba2c61826efe39c255b3e943
Swarajvanchan <swarajvanchan@gmail.com>
2019-10-18 13:19:04 +0530

    DOM

---
+++ DOM/index.js
@@ -0,0 +1,59 @@
+let names=[
+    {
+        os:'android',
+        ver:5.0
+    },
+    {
+        os:'ios',
+        ver:11
+    },
+    {
+        os:'mango',
+        ver:5
+    },
+    {
+        os:'rim',
+        ver:3
+    },
+    {
+        os:'windows IOT',
+        ver:10
+    },
+    {
+        os:'LinuxCNC',
+        ver:5
+    }
+
+]
+let table=document.getElementById('labOs')
+console.log(table)
+let totalCost=0
+
+names.forEach(os=>{
+    let row=document.createElement('tr')
+    row.onclick=()=>{
+        let labOs=document.getElementById('labOs')
+         labOs.textContent=os.os
+         let labVer=document.getElementById('labVer')
+         labVer.textContent=os.ver
+    }
+    let tdOs=-document.createElement('td')
+    tdOs.innerHTML=os.os
+    row.appendChild(tdOs)
+
+    let tdVer=document.createElement('td')
+    tdVer.inner=os.ver
+    row.appendChild(tdVer)
+    let tdCost=document.createElement('td')
+    let inp=document.createElement('input')
+    inp.type='number'
+    inp.onekeyup=ev=>{
+        totalCost=0
+        toatalCost+=parseInt(ev.target.value)
+        console.log(totalCost)
+    }
+    tdCost.appendChild(inp)
+    row.appendChild(tdCost) 
+    table.appendChild(row)
+   })
+   console.log(document)
